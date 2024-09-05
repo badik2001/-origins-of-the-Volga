a60c2f0d483b7383a4ff402b5b08947783c2d993
Daniil <dv.baskov@uokm.ru>
2024-09-05 18:18:18 +0400

    done_navigation

---
+++ wp-content/themes/istoki_volgi/footer.php
@@ -0,0 +1,128 @@
+<footer class="footer scrollme" id="bottom">
+
+
+    <div class="footer_block">
+
+
+        <div class="textcolumn">
+            <a title="Открыть главную страницу" href="https://istoki-volgi.ru/"><br><img src="<?= $get_template_directory_uri; ?>/img/331.png" alt="Логотип"></a>
+            <br>
+            <p>432017, г. Ульяновск, бульвар Новый Венец, 3/4</p>
+            <p>тел. <span class="phone">+7 (8422) 44-30-64</span>, <span class="phone">+7 (8422) 44-30-92</span></p>
+        </div>
+
+        <div class="textcolumn">
+            <div class="footltext textfooter">
+            </div>
+            <br>
+            <div class="socialblock">
+
+                <!--
+					<b>Присоединяйтесь к нам в социальных сетях:</b><br>
+					<a href="https://vk.com/uokm073"><img class="socialicon" src="/img/social/vk.png" alt="ВКонтакте"></a>
+					<a href="https://www.instagram.com/uokm73/"><img class="socialicon" src="/img/social/insta.png" alt="Инстаграм"></a>
+					<a href="https://twitter.com/uokm2"><img class="socialicon" src="/img/social/twitter.png" alt="Твиттер"></a>
+					<a href="https://www.facebook.com/uokm73/?ref=bookmarks"><img class="socialicon" src="/img/social/facebook.png" alt="Фейсбук"></a>
+					<br/><br/>
+		-->
+
+                <script type="text/javascript" src="./Совет музеев ПФО._files/share.js.Без названия" charset="utf-8"></script>
+                Поделитесь нашим сайтом в социальных сетях:<br>
+                <div style="margin-left:auto;margin-right:auto;" class="yashare-auto-init b-share_theme_counter" data-yasharel10n="ru" data-yasharequickservices="yaru,vkontakte,odnoklassniki,moimir" data-yasharetheme="counter" data-yasharetitle="Совет музеев ПФО. " data-yasharedescription="Истоки - Рождённые на Волге. Совместный проект 56 музеев ПФО." data-yasharelink="https://istoki-volgi.ru"><span class="b-share"><span class="b-share-btn__wrap"><a rel="nofollow" target="_blank" title="ВКонтакте" class="b-share__handle b-share__link b-share-btn__vkontakte" href="https://share.yandex.net/go.xml?service=vkontakte&amp;url=https%3A%2F%2Fistoki-volgi.ru&amp;title=%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E.%20&amp;description=%D0%98%D1%81%D1%82%D0%BE%D0%BA%D0%B8%20-%20%D0%A0%D0%BE%D0%B6%D0%B4%D1%91%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BD%D0%B0%20%D0%92%D0%BE%D0%BB%D0%B3%D0%B5.%20%D0%A1%D0%BE%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%2056%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E." data-service="vkontakte"><span class="b-share-icon b-share-icon_vkontakte"></span><span class="b-share-counter">0</span></a></span><span class="b-share-btn__wrap"><a rel="nofollow" target="_blank" title="Одноклассники" class="b-share__handle b-share__link b-share-btn__odnoklassniki" href="https://share.yandex.net/go.xml?service=odnoklassniki&amp;url=https%3A%2F%2Fistoki-volgi.ru&amp;title=%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E.%20&amp;description=%D0%98%D1%81%D1%82%D0%BE%D0%BA%D0%B8%20-%20%D0%A0%D0%BE%D0%B6%D0%B4%D1%91%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BD%D0%B0%20%D0%92%D0%BE%D0%BB%D0%B3%D0%B5.%20%D0%A1%D0%BE%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%2056%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E." data-service="odnoklassniki"><span class="b-share-icon b-share-icon_odnoklassniki"></span><span class="b-share-counter">0</span></a></span><span class="b-share-btn__wrap"><a rel="nofollow" target="_blank" title="Мой Мир" class="b-share__handle b-share__link b-share-btn__moimir" href="https://share.yandex.net/go.xml?service=moimir&amp;url=https%3A%2F%2Fistoki-volgi.ru&amp;title=%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E.%20&amp;description=%D0%98%D1%81%D1%82%D0%BE%D0%BA%D0%B8%20-%20%D0%A0%D0%BE%D0%B6%D0%B4%D1%91%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BD%D0%B0%20%D0%92%D0%BE%D0%BB%D0%B3%D0%B5.%20%D0%A1%D0%BE%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%2056%20%D0%BC%D1%83%D0%B7%D0%B5%D0%B5%D0%B2%20%D0%9F%D0%A4%D0%9E." data-service="moimir"><span class="b-share-icon b-share-icon_moimir"></span><span class="b-share-counter">0</span></a></span><iframe style="display: none" src="./Совет музеев ПФО._files/ya-share-cnt.html"></iframe></span></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+
+
+        <div class="messagecolumn">
+            <h3>
+                Напишите нам сообщение
+            </h3>
+            <form>
+                <img src="<?= $get_template_directory_uri; ?>/imgman.svg" alt="name">
+                <input id="footname-field" type="text" name="footname" title="Ваше имя" placeholder="Ваше имя" value="">
+                <div class="clear mb10"></div>
+
+                <img src="<?= $get_template_directory_uri; ?>/imgphone.svg" alt="phone">
+                <input id="footphone-field" type="text" name="footphone" title="Телефон" placeholder="Телефон" value="">
+                <div class="clear mb10"></div>
+
+                <img src="<?= $get_template_directory_uri; ?>/imgmail.svg" alt="email">
+                <input id="footemail-field" type="text" name="footemail" title="E-mail" placeholder="E-mail" value="">
+                <div class="clear mb10"></div>
+
+                <img src="<?= $get_template_directory_uri; ?>/imgtext.svg" alt="text">
+                <textarea id="footmessage-field" name="footmessage" title="Сообщение" placeholder="Сообщение"></textarea>
+                <div class="clear mb10"></div>
+
+                <div class="osquare button">
+                    <a id="footbackmessage" href="https://istoki-volgi.ru/#sendmessage" title="Отправить сообщение"><img src="<?= $get_template_directory_uri; ?>/img/round-mail-24px-white.svg" alt="Отправить"> Отправить</a>
+                </div>
+            </form>
+        </div>
+
+    </div> <!-- anima div-->
+
+</footer> <!-- bottom -->
+
+<!--<script type="text/javascript" src='/js/prefixfree.min.js'></script>-->
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    ym(51605393, "init", {
+        id: 51605393,
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true
+    });
+</script> <noscript>
+    <div><img src="https://mc.yandex.ru/watch/51605393" style="position:absolute; left:-9999px;" alt="" /></div>
+</noscript> <!-- /Yandex.Metrika counter -->
+<a id="lbLauncher" style="display: none;"></a>
+<div id="lbOverlay" class="grey" style="display: none;"></div>
+<div id="lbMain" style="display: none;">
+    <div id="lbOuterContainer" class="grey">
+        <div id="lbIframeContainer" style="display: none;"><iframe id="lbIframe" name="lbIframe" frameborder="0" class="grey" style="display: none;" src="./Совет музеев ПФО._files/saved_resource.html"></iframe></div>
+        <div id="lbImageContainer"><img id="lbImage" class="grey">
+            <div id="lbHoverNav"><a id="lbPrev" class="grey" href="javascript:void(0)"></a><a id="lbNext" class="grey" href="javascript:void(0)"></a></div>
+        </div>
+        <div id="lbLoading" class="grey"></div>
+        <div id="lbDetailsContainer" class="grey">
+            <div id="lbDetailsData" class="grey">
+                <div id="lbDetails"><span id="lbCaption"></span><span id="lbNumberDisplay"></span><span id="lbNavDisplay" style="display: none;"></span></div>
+                <div id="lbBottomNav"><a id="lbClose" class="grey" href="javascript:void(0)"></a><a id="lbPrint" class="grey" href="javascript:void(0)" style="display: none;"></a><a id="lbPause" class="grey" href="javascript:void(0)" style="display: none;"></a><a id="lbPlay" class="grey" href="javascript:void(0)" style="display: none;"></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="highslide-container" style="padding: 0px; border: none; margin: 0px; position: absolute; left: 0px; top: 0px; width: 100%; z-index: 1001; direction: ltr;"><a class="highslide-loading" title="Click to cancel" href="javascript:;" style="position: absolute; top: -9999px; opacity: 0.75; z-index: 1;">Loading...</a>
+    <div style="display: none;"></div>
+    <table cellspacing="0" style="padding: 0px; border: none; margin: 0px; visibility: hidden; position: absolute; border-collapse: collapse; width: 0px;">
+        <tbody style="padding: 0px; border: none; margin: 0px;">
+            <tr style="padding: 0px; border: none; margin: 0px; height: auto;">
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) 0px 0px; height: 20px; width: 20px;"></td>
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) 0px -40px; height: 20px; width: 20px;"></td>
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) -20px 0px; height: 20px; width: 20px;"></td>
+            </tr>
+            <tr style="padding: 0px; border: none; margin: 0px; height: auto;">
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) 0px -80px; height: 20px; width: 20px;"></td>
+                <td class="drop-shadow highslide-outline" style="padding: 0px; border: none; margin: 0px; position: relative;"></td>
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) -20px -80px; height: 20px; width: 20px;"></td>
+            </tr>
+            <tr style="padding: 0px; border: none; margin: 0px; height: auto;">
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) 0px -20px; height: 20px; width: 20px;"></td>
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) 0px -60px; height: 20px; width: 20px;"></td>
+                <td style="padding: 0px; border: none; margin: 0px; line-height: 0; font-size: 0px; background: url(&quot;https://istoki-volgi.ru/js/highslide/graphics/outlines/drop-shadow.png&quot;) -20px -20px; height: 20px; width: 20px;"></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+
+</html>
